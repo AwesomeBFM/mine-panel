fb5a8ba3ef30090798c157ad50103a4b8ad7b87c
AwesomeBFM <55819926+AwesomeBFM@users.noreply.github.com>
2023-08-22 14:00:18 -0600

    Added rows to recent orders

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+enum Provider {
+  VISA,
+  APPLE,
+  GOOGLE,
+  MASTERCARD,
+  DISCOVER,
+  PAYPAL,
+  AMEX
+}
+
+enum Status {
+  SUCCESS,
+  FAILED,
+  PENDING,
+  REFUNDED,
+  CHARGEBACK
+}
+
 interface Order {
   id: string;
+  timestamp: string;
   username: string;
   pfpRoute: string;
+  status: Status;
   total: string;
+  method: Provider;
   items: string;
 }
 
@@ -16,12 +37,67 @@ interface Order {
 export class OrdersComponent {
   orders: Order[] = [
     {
-      id: '1',
-      username: 'bidenman',
+      id: '1234',
+      timestamp: '14:32 8/22/23',
+      username: 'Joe Biden',
       pfpRoute: '/assets/pfp.jpg',
-      total: '$53.28',
-      items: '2'
+      status: Status.SUCCESS,
+      total: '$53.23',
+      method: Provider.APPLE,
+      items: '4'
+    },
+    {
+      id: '1234',
+      timestamp: '14:32 8/22/23',
+      username: 'Joe Biden',
+      pfpRoute: '/assets/pfp.jpg',
+      status: Status.FAILED,
+      total: '$53.23',
+      method: Provider.APPLE,
+      items: '4'
+    },
+    {
+      id: '1234',
+      timestamp: '14:32 8/22/23',
+      username: 'Joe Biden',
+      pfpRoute: '/assets/pfp.jpg',
+      status: Status.PENDING,
+      total: '$53.23',
+      method: Provider.APPLE,
+      items: '4'
+    },
+    {
+      id: '1234',
+      timestamp: '14:32 8/22/23',
+      username: 'Joe Biden',
+      pfpRoute: '/assets/pfp.jpg',
+      status: Status.REFUNDED,
+      total: '$53.23',
+      method: Provider.APPLE,
+      items: '4'
+    },
+    {
+      id: '1234',
+      timestamp: '14:32 8/22/23',
+      username: 'Joe Biden',
+      pfpRoute: '/assets/pfp.jpg',
+      status: Status.CHARGEBACK,
+      total: '$53.23',
+      method: Provider.APPLE,
+      items: '4'
+    },
+    {
+      id: '1234',
+      timestamp: '14:32 8/22/23',
+      username: 'Joe Biden',
+      pfpRoute: '/assets/pfp.jpg',
+      status: Status.SUCCESS,
+      total: '$53.23',
+      method: Provider.APPLE,
+      items: '4'
     }
   ];
 
+  protected readonly Status = Status;
+  protected readonly Provider = Provider;
 }
